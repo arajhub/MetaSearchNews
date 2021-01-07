3da0fbc95e002a82141f5467763b99854caa736d
Nitesh <rananitesh8@gmail.com>
2021-01-08 00:58:38 +0530

    added best ranking still in progress

--- news_main.py
+++ news_main.py
@@ -1,5 +1,7 @@
+from all_news import AllNews
 from query_newsapi import query_newsapi
 from query_google import query_google_news
+import pandas as pd
 
 
 def search_news():
@@ -7,9 +9,9 @@ def search_news():
     print("Welcome to Meta News Search")
     query = input("Input your search query")
     result_from_all = query_newsapi(query) + query_google_news(query)
-    for res in result_from_all:
-        print(res)
-
+    # for res in result_from_all:
+    #     print(res)
+    best_rank(result_from_all)
     # TODO:
 
     '''
@@ -23,8 +25,15 @@ def search_news():
     // MetaRank refers rank assigned by meta Search engine
     
     '''
-    def best_rank(all_search_result):
-        raise NotImplementedError
+
+
+def best_rank(all_search_result):
+    df = pd.DataFrame([t.__dict__ for t in all_search_result])
+    grouped_by_title = df.groupby('title')
+    df["rank_column"] = grouped_by_title[["rank"]].transform(min)
+    df = df.sort_values("rank_column", ascending=True)
+    df = df.drop("rank_column", axis=1)
+    print(df)
 
     '''
     b) Approach 2:
@@ -32,8 +41,10 @@ def search_news():
     Collection (Taks2)
     Write the results in the document named ResultantRanks_A2.txt
     '''
-    def custom_rank(all_search_result):
-        raise NotImplementedError
+
+
+def custom_rank(all_search_result):
+    raise NotImplementedError
 
 
 if __name__ == '__main__':
